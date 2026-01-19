fc1f092e10ee9ff177d85b074412144810a7cfbb
Yousif Al-Khoury <59463563+YousifAlKhoury@users.noreply.github.com>
2026-01-19 01:28:18 -0500

    Uncomment print statements for Phys and Frangi losses

--- nnunetv2/training/loss/compound_losses.py
+++ nnunetv2/training/loss/compound_losses.py
@@ -187,9 +187,9 @@ class VeinPhysics_Frangi_DC_and_CE_loss(nn.Module):
                     
         # print("CE loss: ", ce_loss)
         print("DC loss: ", dc_loss)
-        # print("Phys loss: ", phys_loss)
+        print("Phys loss: ", phys_loss)
         # print("Tversky: ", tversky_loss)
-        # print("Frangi: ", frangi_loss)
+        print("Frangi: ", frangi_loss)
 
         return total
 
