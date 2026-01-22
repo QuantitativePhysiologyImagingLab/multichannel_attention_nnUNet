1b074502c32c75d9abdb65984d62cd766e853dd6
Yousif Al-Khoury <59463563+YousifAlKhoury@users.noreply.github.com>
2026-01-22 11:44:55 -0500

    Update nnUNetTrainerFrangiPhysicsWithAttention.py

--- nnunetv2/training/nnUNetTrainer/nnUNetTrainerFrangiPhysicsWithAttention.py
+++ nnunetv2/training/nnUNetTrainer/nnUNetTrainerFrangiPhysicsWithAttention.py
@@ -610,11 +610,11 @@ class nnUNetTrainerFrangiPhysicsWithAttention(nnUNetTrainer):
                             'do_bg': True, 'smooth': 1e-5, 'ddp': self.is_ddp},
                             {},
                             {},
-                            weight_ce=1, 
-                            weight_dice=0.5, 
-                            weight_tversky=1, 
+                            weight_ce=2, 
+                            weight_dice=1, 
+                            weight_tversky=0.25, 
                             weight_physics=20, 
-                            weight_frangi=1,
+                            weight_frangi=0.1,
                             ignore_label=self.label_manager.ignore_label,
                             dice_class=MemoryEfficientSoftDiceLoss)
 
