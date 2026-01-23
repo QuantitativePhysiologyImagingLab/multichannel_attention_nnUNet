65f21cfc2d0ee70dd199b71acb2cbc4385e849d3
Yousif Al-Khoury <59463563+YousifAlKhoury@users.noreply.github.com>
2026-01-23 01:07:49 -0500

    Update compound_losses.py

--- nnunetv2/training/loss/compound_losses.py
+++ nnunetv2/training/loss/compound_losses.py
@@ -193,11 +193,11 @@ class VeinPhysics_Frangi_DC_and_CE_loss(nn.Module):
             #           "requires_grad=", total.requires_grad,
             #           flush=True)
                     
-        print("CE loss: ", ce_loss)
-        print("DC loss: ", dc_loss)
-        print("Phys loss: ", phys_loss)
-        print("Tversky: ", tversky_loss)
-        print("Frangi: ", frangi_loss)
+        # print("CE loss: ", ce_loss)
+        # print("DC loss: ", dc_loss)
+        # print("Phys loss: ", phys_loss)
+        # print("Tversky: ", tversky_loss)
+        # print("Frangi: ", frangi_loss)
 
         return total
 
