45f5664c1b15a4e942869ee343928539f8d07be8
yousifAlKhoury <yousif.alkhoury@gmail.com>
2026-04-28 02:16:51 -0400

    more frangi weight

--- nnunetv2/training/nnUNetTrainer/nnUNetTrainerFrangiPhysicsWithAttention.py
+++ nnunetv2/training/nnUNetTrainer/nnUNetTrainerFrangiPhysicsWithAttention.py
@@ -618,8 +618,8 @@ class nnUNetTrainerFrangiPhysicsWithAttention(nnUNetTrainer):
                             weight_ce=1,
                             weight_dice=0.5,
                             weight_tversky=1,
-                            weight_physics=3,
-                            weight_frangi=0.3,
+                            weight_physics=0,
+                            weight_frangi=0.5,
                             weight_volume=0.0,
                             ignore_label=self.label_manager.ignore_label,
                             dice_class=MemoryEfficientSoftDiceLoss)
